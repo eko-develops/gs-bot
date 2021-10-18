7762263ff3bf8848ac298b9aed68b08bb20095f2
eko-develops <ekodevelops@gmail.com>
2021-10-18 15:02:32 -0700

    Add comments. Remove mongodb connection file

--- classes/Database.js
+++ classes/Database.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 
 class Database {
 
-    #mongoSRV;
+    #mongoSRV;  //set this to a private variable so we cant access it directly
 
     constructor (mongoSRV) {
         this.#mongoSRV = mongoSRV;
     }
 
-    #activeMongoSRV(){
+    #activeMongoSRV(){  //use a private method so we can only use this within the class
         return this.#mongoSRV;
     }
 
+    //starts the connection to the database
     start () {
         mongoose.connect(this.#activeMongoSRV(),{
             useNewUrlParser: true,
