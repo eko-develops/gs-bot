342a7ac3f115936c6930dd00598b776605162f2b
eko-develops <ekodevelops@gmail.com>
2021-10-18 16:15:54 -0700

    Change text for ready event

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     once: true,
     execute (client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+        console.log(`READY EVENT FIRED! Logged in as ${client.user.tag}`);
     },
 };
