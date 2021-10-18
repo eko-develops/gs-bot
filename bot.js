018224121607efcdd9da898b9757e86b568f0f0e
eko-develops <ekodevelops@gmail.com>
2021-10-18 16:18:54 -0700

    Move interaction create into event handler

--- bot.js
+++ bot.js
@@ -4,19 +4,3 @@ const client = new Client(process.env.MONGODB_SRV);	//create a client instance w
 
 client.botStart();	//starts client connection from Client class
 client.dbStart(); //starts the database connection from Database class that is accessable through the client class
-
-//The clients listens on the event that a message is sent from any channel
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);	//fetch the command in the collection
-
-	if (!command) return;	//if the command doesnt exist, exit
-
-	try {
-		await command.execute(interaction);	//if the command does exist, call the commands execute method and pass in the interaction
-	} catch (error) {	//catch any errors and reply back with an error
-		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,18 @@
 module.exports = {
     name: 'interactionCreate',
-    execute (interaction) {
+    async execute (interaction) {
         console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+        if (!interaction.isCommand()) return;
 
+        const command = interaction.client.commands.get(interaction.commandName);	//fetch the command in the collection
+    
+        if (!command) return;	//if the command doesnt exist, exit
+    
+        try {
+            await command.execute(interaction);	//if the command does exist, call the commands execute method and pass in the interaction
+        } catch (error) {	//catch any errors and reply back with an error
+            console.error(error);
+            return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
     }
 };
