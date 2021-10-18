796e1570bbb57b13cf09466fe34f86b00edd3a2f
eko-develops <ekodevelops@gmail.com>
2021-10-18 13:59:02 -0700

    Re-write how commands and events files are read

--- bot.js
+++ bot.js
@@ -11,36 +11,45 @@ const fs = require('fs');	//used to read commands folder
 // eslint-disable-next-line no-undef
 client.commands = new Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter( file => file.endsWith('.js')); //read the commands folder and filter out files that dont have js extenstion
-
-//Set commands to the client.commands collection
-for (const file of commandFiles) {
-
+fs.readdirSync('./commands').filter( file => file.endsWith('.js')).forEach( file => {
 	const command = require(`./commands/${file}`);
 
+	console.log(`Command ${command} loaded`);
 	client.commands.set(command.data.name, command);
-}
+}); //read the commands folder, filter any files that dont end with js, 
+
+fs.readdirSync('./events').filter( file => file.endsWith('.js')).forEach( file => {
+	const event = require(`./events/${file}`);
+
+	console.log(`Event ${event} loaded`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}); //read the events folder, filter out files that dont have js extenstion, depending on the event type, fire it
+
 
 const connectDb = require('./mongodb.js');
 
 connectDb();
 
 //When client is ready and connected
-client.once('ready', () => {
-	console.log('GSBOT is logged in..');
-});
+// client.once('ready', () => {
+// 	console.log('GSBOT is logged in..');
+// });
 
 //The clients listens on the event that a message is sent from any channel
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+	const command = client.commands.get(interaction.commandName);	//fetch the command in the collection
 
-	if (!command) return;
+	if (!command) return;	//if the command doesnt exist, exit
 
 	try {
-		await command.execute(interaction);
-	} catch (error) {
+		await command.execute(interaction);	//if the command does exist, call the commands execute method and pass in the interaction
+	} catch (error) {	//catch any errors and reply back with an error
 		console.error(error);
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
